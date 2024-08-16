82ffef001059dd3afe2b3408ab27b1dd7cec495e
okt4hei <142867353+okt4hei@users.noreply.github.com>
2024-08-17 00:08:04 +0900

    検索クエリ・結果をvalueNotifierで管理

--- lib/ui/organisms/search_options.dart
+++ lib/ui/organisms/search_options.dart
@@ -1,94 +1,128 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:search_github_repository/constants/sort_options.dart';
+import 'package:search_github_repository/model/query_options.dart';
+import 'package:search_github_repository/model/search_result.dart';
 import 'package:search_github_repository/ui/atoms/pagination.dart';
 import 'package:search_github_repository/ui/molecules/search.dart';
 import 'package:search_github_repository/ui/molecules/sort.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
-class SearchOptions extends StatelessWidget {
+class SearchOptions extends StatefulWidget {
   const SearchOptions({
     super.key,
     required this.onChanged,
-    required this.query,
-    required this.selected,
-    required this.ascending,
-    required this.pageLength,
-    required this.currentPage,
+    required this.searchResultNotifier,
   });
 
   /// 検索条件が変更されたときに呼ばれるコールバック
   /// onChanged(String query, String sort, bool ascending, int page)
-  final void Function(String, String, bool, int) onChanged;
+  final void Function(QueryOptions) onChanged;
 
-  /// 検索クエリ
-  final String query;
+  /// 直前の検索結果
+  final ValueNotifier<SearchResult> searchResultNotifier;
 
-  /// ソートオプション
-  final String selected;
+  @override
+  State<SearchOptions> createState() => SearchOptionsState();
+}
 
-  /// 昇順かどうか
-  final bool ascending;
+class SearchOptionsState extends State<SearchOptions> {
+  final TextEditingController controller = TextEditingController();
+  QueryOptions get queryOptions =>
+      widget.searchResultNotifier.value.queryOptions;
+  Future<int> get pageLength => widget.searchResultNotifier.value.pageLength;
 
-  /// ページ数
-  final int pageLength;
+  final List<String> sortOptions = SortOptions.values
+      .map((option) => option.name)
+      .toList(); // Best Match, Star, Fork
 
-  /// 現在のページ
-  final int currentPage;
-
-  onQuerySubmitted(String query) {
-    if (query == this.query) return;
-    onChanged(query, selected, ascending, currentPage);
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
-  onSortOptionChanged(String selected, bool ascending) {
-    if (selected == SortOptions.bestMatch.name) {
+  onQuerySubmitted(String query) {
+    if (query.isEmpty || queryOptions.query == query) return;
+    widget.onChanged(QueryOptions(
+      query: query,
+      sort: queryOptions.sort,
+      ascending: queryOptions.ascending,
+      page: 0, // クエリが変わったらページをリセット
+    ));
+  }
+
+  onSortOptionChanged(String sort, bool ascending) {
+    if (sort == SortOptions.bestMatch.name) {
       ascending = false; // Best matchは必ず降順
     }
-    onChanged(query, selected, ascending, currentPage);
+    widget.onChanged(QueryOptions(
+      query: queryOptions.query,
+      sort: sort,
+      ascending: ascending,
+      page: 0, // ソートが変わったらページをリセット
+    ));
   }
 
   onPageChanged(int page) {
-    onChanged(query, selected, ascending, page);
+    widget.onChanged(QueryOptions(
+      query: queryOptions.query,
+      sort: queryOptions.sort,
+      ascending: queryOptions.ascending,
+      page: page,
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    final List<String> sortOptions = SortOptions.values
-        .map((option) => option.name)
-        .toList(); // Best Match, Star, Fork
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Search(
-          onSubmitted: onQuerySubmitted,
-          isMain: false,
-          initiaiValue: query,
-        ),
-        SizedBox(height: 10.r),
-        Wrap(
-          alignment: WrapAlignment.center,
-          crossAxisAlignment: WrapCrossAlignment.center,
-          children: [
-            IntrinsicWidth(
-              child: Sort(
-                options: sortOptions,
-                onChanged: onSortOptionChanged,
-                selected: sortOptions.indexOf(selected),
-                ascending: ascending,
-                showOrderChoice: selected !=
-                    SortOptions.bestMatch.name, // Best matchはソート順を選択できない
+    return ValueListenableBuilder(
+        valueListenable: widget.searchResultNotifier,
+        builder: (_, __, ___) {
+          controller.text = queryOptions.query;
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Search(
+                onSubmitted: onQuerySubmitted,
+                isMain: false,
+                controller: controller,
               ),
-            ),
-            Pagination(
-              length: pageLength,
-              currentPage: currentPage,
-              onPageChanged: onPageChanged,
-            ),
-          ],
-        ),
-      ],
-    );
+              SizedBox(height: 10.r),
+              Wrap(
+                alignment: WrapAlignment.center,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  IntrinsicWidth(
+                    child: Sort(
+                      options: sortOptions,
+                      onChanged: onSortOptionChanged,
+                      selected: sortOptions.indexOf(queryOptions.sort),
+                      ascending: queryOptions.ascending,
+                      showOrderChoice: queryOptions.sort !=
+                          SortOptions.bestMatch.name, // Best matchはソート順を選択できない
+                    ),
+                  ),
+                  FutureBuilder(
+                    future: pageLength,
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState != ConnectionState.done) {
+                        return const SizedBox.shrink();
+                      }
+                      return Pagination(
+                        length: snapshot.data!,
+                        currentPage: queryOptions.page,
+                        onPageChanged: onPageChanged,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
   }
 }
 
@@ -98,17 +132,15 @@ class SearchOptions extends StatelessWidget {
   path: 'organisms',
 )
 Widget searchOptions(BuildContext context) {
-  ValueNotifier<List<String>> notifier = ValueNotifier([
-    '',
-    SortOptions.bestMatch.name,
-    'false',
-    '0',
-  ]);
+  ValueNotifier<SearchResult> notifier = ValueNotifier(SearchResult.template);
 
-  onChanged(String query, String sort, bool ascending, int page) {
-    debugPrint(
-        'query: $query, sort: $sort, ascending: $ascending, page: $page');
-    notifier.value = [query, sort, ascending.toString(), page.toString()];
+  onChanged(QueryOptions query) {
+    debugPrint('query: $query');
+    notifier.value = SearchResult(
+      queryOptions: query,
+      pageLength: Future.value(10),
+      repositories: Future.value([]),
+    );
   }
 
   return Container(
@@ -117,18 +149,9 @@ Widget searchOptions(BuildContext context) {
       child: Column(
         children: [
           const Spacer(),
-          ValueListenableBuilder<List<String>>(
-            valueListenable: notifier,
-            builder: (context, value, _) {
-              return SearchOptions(
-                onChanged: onChanged,
-                query: value[0],
-                selected: value[1],
-                ascending: value[2] == 'true',
-                currentPage: int.parse(value[3]),
-                pageLength: 10,
-              );
-            },
+          SearchOptions(
+            onChanged: onChanged,
+            searchResultNotifier: notifier,
           ),
           const Spacer(),
         ],
